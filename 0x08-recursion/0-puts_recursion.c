634cf19f327be27030a2757fc26160e0bb2a574b
Akinyemi <akinyemioreitan@gmail.com>
2023-01-05 15:42:19 -0800

    task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,15 @@
-#include "main.h"
 /**
-* _puts_recursion - function like puts();
-* @s: input
-* Return: Always 0 (Success)
-*/
-void _puts_recursion(char *s)
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+Lvoid _puts_recursion(char *s)
 {
-if (*s)
+  if (*s)
 {
-_putchar(*s);
- _puts_recursion(s + 1);
+ _putchar(*s);
+_puts_recursion(s + 1);
 }
 else
 _putchar('\n');
-}
+}                                                                                                          
